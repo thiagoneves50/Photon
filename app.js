10ee26674f5c201dba5aeed91a5e6bf7e8093c43
Thiago Neves <thiagoneves50@gmail.com>
2022-01-15 11:17:14 -0300

    Nothing found message if no itens are found

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const auth = "563492ad6f9170000100000149a3e1fb8c7145f7b10fcb45411de2cd";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
 const form = document.querySelector(".search-form");
+const moreBtn = document.querySelector(".more");
+
+let pageCount = 1;
 let searchValue;
 
 // Event Listeners
 
 searchInput.addEventListener("input", updateValue);
 form.addEventListener("submit", searchPhotos);
+moreBtn.addEventListener("click", loadMoreItmes);
 
 // Functions
 
@@ -17,6 +21,15 @@ function clear() {
 
 function updateValue(e) {
 	searchValue = e.target.value;
+}
+
+function nothingFound() {
+	const divNothingFound = document.createElement("div");
+	divNothingFound.innerHTML = /*html*/ `
+		<p>Sorry, nothing was found. Please try another term.</p>	
+	`;
+	divNothingFound.classList.add("nothing-found");
+	gallery.appendChild(divNothingFound);
 }
 
 function renderImages(photosArr) {
@@ -54,13 +67,41 @@ async function curatedPhotos() {
 }
 
 async function searchPhotos(e) {
+	pageCount = 1;
 	e.preventDefault();
 	clear();
-	const data = await fetchPhotos(
+	const data = await axiosFetch(
 		`https://api.pexels.com/v1/search?query=${searchValue}&per_page=15`
 	);
 
-	renderImages(data);
+	if (data.data.total_results === 0) {
+		nothingFound();
+		moreBtn.classList.add("hide-btn");
+	} else {
+		renderImages(data.data);
+		moreBtn.classList.remove("hide-btn");
+	}
 }
 
-curatedPhotos();
+// Using Axios
+
+async function axiosFetch(url) {
+	const data = await axios.get(url, {
+		method: "GET",
+		headers: {
+			Accept: "application/json",
+			Authorization: auth,
+		},
+	});
+	return data;
+}
+
+async function loadMoreItmes(count) {
+	pageCount++;
+	const data = await axiosFetch(
+		`https://api.pexels.com/v1/search?query=${searchValue}&per_page=15&page=${pageCount}`
+	);
+
+	renderImages(data.data);
+	console.log(data.data);
+}
